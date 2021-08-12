ebf1566e37e2aecd05d253e13352e24bef0fbfde
maroon <maroonay@gmail.com>
2021-08-12 22:10:54 +0300

    updated transport interface

--- pkg/transport/transport.go
+++ pkg/transport/transport.go
@@ -4,4 +4,6 @@ package transport
 type Transport interface {
 	SendAsync(id string, msgType string, version string, payload []byte)
 	GetVersion(id string, msgType string) string
+	Start()
+	Stop()
 }
