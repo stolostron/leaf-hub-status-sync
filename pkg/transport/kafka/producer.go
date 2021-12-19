5b9627586e8ed86263887056a7345682b68dad37
Maroon Ayoub <maroon.ayoub@ibm.com>
2021-12-19 07:06:26 -0800

    Added transportation key to delta bundles that is used as postfix to message-key when syncing delta bundles. (#23)
    * Added transportation key to delta bundles that is used as postfix to message-key when syncing delta bundles.
    
    * minor refactor

--- pkg/transport/kafka/producer.go
+++ pkg/transport/kafka/producer.go
@@ -219,9 +219,9 @@ func (p *Producer) SendAsync(message *transport.Message) {
 		{Key: headers.CompressionType, Value: []byte(p.compressor.GetType())},
 	}
 
-	if err = p.kafkaProducer.ProduceAsync(message.ID, p.topic, partition, messageHeaders, compressedBytes); err != nil {
-		p.log.Error(err, "failed to send message", "MessageId", message.ID, "MessageType",
-			message.MsgType, "Version", message.Version)
+	if err = p.kafkaProducer.ProduceAsync(message.Key, p.topic, partition, headers, compressedBytes); err != nil {
+		p.log.Error(err, "failed to send message", "MessageKey", message.Key, "MessageId", message.ID,
+			"MessageType", message.MsgType, "Version", message.Version)
 		transport.InvokeCallback(p.eventSubscriptionMap, message.ID, transport.DeliveryFailure)
 
 		return
@@ -229,6 +229,6 @@ func (p *Producer) SendAsync(message *transport.Message) {
 
 	transport.InvokeCallback(p.eventSubscriptionMap, message.ID, transport.DeliveryAttempt)
 
-	p.log.Info("message sent to transport server", "MessageId", message.ID, "MessageType",
-		message.MsgType, "Version", message.Version)
+	p.log.Info("message sent to transport server", "MessageKey", message.Key, "MessageId", message.ID,
+		"MessageType", message.MsgType, "Version", message.Version)
 }
